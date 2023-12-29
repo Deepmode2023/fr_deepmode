88d47f30292640c98bfacb67a63e04d4feee654f
AlekseyLebedik <lebedik.leha@gmail.com>
2023-12-29 03:31:17 +0200

    .

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,5 +1,6 @@
 export const parseJwt = (token: string) => {
   const base64Payload = token.split(".")[1];
+
   const payload = Buffer.from(base64Payload, "base64");
   return JSON.parse(payload.toString());
 };
